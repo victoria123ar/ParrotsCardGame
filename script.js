6a08c1b08e1fd16bfb4a5d2fd2a28dfff11e5757
Victoria <vic_andrade_@hotmail.com>
2022-10-19 17:15:41 -0300

    Versão com html e css completos

---
+++ script.js
@@ -0,0 +1,14 @@
+let contador = prompt("Com quantas cartas deseja jogar? (De 4 a 5 cartas)");
+const cartas = document.querySelector(".cartas");
+const personagens = [
+  "bobrossparrot",
+  "exexplodyparrot",
+  "fiestaparrot",
+  "metalparrot",
+  "revertitparrot",
+  "tripletsparrot",
+  "unicornparrot",
+];
+const clicks = 0;
+const relogio = document.querySelector(".relogio");
+
